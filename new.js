0a2186c952d1e241188af0ba6fa0476ef4999c5a
x64 <alberto.arias.x64@gmail.com>
2022-12-06 16:16:52 -0500

    adjust movement

--- new.js
+++ new.js
@@ -1,16 +1,17 @@
 const slides = document.querySelectorAll('.slides')
 const buttons = document.querySelectorAll('.carrousel-button')
-buttons.forEach(element => {
-    element.addEventListener('click', ({target}) => move_slider(target))
-})
+buttons.forEach(element => element.addEventListener('click', ({target}) => move_slider(target)))
 
                 /* card + gap */
 const slide_width = 300 + 20
 const margin = 120
-let delta_scroll = [0,0,0,0,0,0,0,0,0,0]
+let delta_scroll = []
 let slides_view = parseInt((window.visualViewport.width - margin)/slide_width)
 
-slides.forEach(element => element.style.width = `${slide_width * slides_view}px`)
+slides.forEach(element => {
+    element.style.width = `${slide_width * slides_view}px`
+    delta_scroll.push(0)
+})
 
 window.onresize = () => {
     slides_view = parseInt((window.visualViewport.width - margin)/slide_width)
@@ -25,7 +26,7 @@ const move_slider = (target) => {
     container.forEach(element => {
         if (element.dataset.id == container_id) slides_container = element
     })
-    if (move) delta_scroll[container_id] -= slide_width
+    if (move === 'false') delta_scroll[container_id] -= slide_width
     else delta_scroll[container_id] += slide_width
     if ((delta_scroll[container_id] * -1) < 0) delta_scroll[container_id] = 0
     if ((delta_scroll[container_id] * -1) > ((slides_container.childElementCount - slides_view) * slide_width)) delta_scroll[container_id] = 0
